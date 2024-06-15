fc4e3cccebbc671616338e9a1f1f10e8cb71bbe0
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-06-14 23:16:55 -0700

    Not trying to use warchest and bet_value as static variables for now

--- blackjack/currency.py
+++ blackjack/currency.py
@@ -1,20 +1,18 @@
-warchest = 100
-bet_value = 10
-
-def bet():
-    bet_value = input(f"Choose how much to bet, from 10 to your total of {warchest}.")
+def bet(warchest, bet_value):
+    bet_value = int(input(f"Choose how much to bet, from 10 to your total of {warchest}. > "))
+    print(int(bet_value))
     if bet_value > warchest:
         bet_value = warchest
     elif bet_value < 10:
         bet_value = 10
-    elif bet_value is not type(int):
-        bet_value = 10
+    return bet_value
+    
 
 
-def win():
+def win(warchest, bet_value):
     warchest += bet_value
     print(f"Your victory when betting ${bet_value} brings you to ${warchest} total!")
 
-def lose():
+def lose(warchest, bet_value):
     warchest -= bet_value
     print(f"You lost ${bet_value}, leaving you with just ${warchest} left.")
