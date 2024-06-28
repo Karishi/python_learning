015d1cb96edd5ad44d93cb56c190ba3a786682d5
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-06-27 20:40:20 -0700

    Testing the discard function to ensure it's both adding the discarded cards to the discard pile and removing them from the play area.

--- blackjack/test/test_blackjack.py
+++ blackjack/test/test_blackjack.py
@@ -1,6 +1,6 @@
 import pytest
 from src.currency import win, lose
-from src.blackjack_main import choose_hit_or_stay
+from src.blackjack_main import choose_hit_or_stay, discard
 
 def test_win():
     value = win(100,10)
@@ -13,3 +13,10 @@ def test_lose():
 def test_choose_hit_or_stay():
     victory = choose_hit_or_stay(blackjack_main.stay)
     assert victory == None
+
+def test_discard():
+    player_cards = {'King of Hearts': 10, 'Queen of Spades': 10, 'Ace of Hearts': 1}
+    dealer_cards = {'2 of Diamonds': 2, '8 of Hearts': 8, '8 of Clubs': 8}
+    discard_copy = {**player_cards, **dealer_cards}
+    discard_pile = discard(player_cards, dealer_cards)
+    assert discard_pile == discard_copy
