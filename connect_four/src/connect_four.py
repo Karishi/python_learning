a8fb290f25ca4848bc7f5578b264ca5eaa917feb
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-07-10 23:51:18 -0700

    Process of splitting out victory-condition test into its parts has begun, and piece location has been made an object for easier access.

--- connect_four/src/connect_four.py
+++ connect_four/src/connect_four.py
@@ -12,6 +12,13 @@ class player:
 p1 = player("A", 0, 0)
 p2 = player("B", 0, 0)
 
+class piece_location:
+    def __init__(self, column, row, current_player, array2D):
+        self.column = column
+        self.row = row
+        self.player = current_player
+        self.board = array2D
+
 current_player = p2
 
 def display_board():
@@ -27,40 +34,41 @@ def swap_turns(current_player):
     current_player.turn += 1
     return current_player
 
-def drop_piece(array2D, column, current_player):
+def drop_piece(loc):
     column -= 1
-    if array2D[0][column] != 'O':
+    if loc.array2D[0][loc.column] != 'O':
         print("That space is illegal. Please choose another.")
         return False, -1
     else:
         for row in range(0, 6):
             if row == 5:
-                array2D[row][column] = current_player.title
+                loc.array2D[row][loc.column] = loc.current_player.title
                 return True, row
             if array2D[row+1][column] != 'O':
                 array2D[row][column] = current_player.title
                 return True, row
     return True, row
 
-def check_for_victory(array2D, row, column, player_turn):
-    column -= 1
-    dist = 1 
-    connect = 1
-    print(f"Latest piece dropped into {column},{row}")
-    # Check vertical
-    if row < 3: # Can only happen with a piece placed in the top 3 rows
+def victory_vertical(loc):
+    if loc.row < 3: # Can only happen with a piece placed in the top 3 rows
         for i in range(0, 4):
-            print(f"Piece below is {column},{row+dist} and is {array2D[row+dist][column]}.")
-            if row+dist <= 5 and array2D[row+dist][column] == player_turn:
+            print(f"Piece below is {loc.column},{loc.row+dist} and is {loc.array2D[loc.row+dist][loc.column]}.")
+            if loc.row+dist <= 5 and loc.array2D[loc.row+dist][loc.column] == loc.player.title:
                 dist += 1
                 connect += 1
                 print("+1 on the vertical")
                 if connect == 4:
                     return True
             else:
-                dist = 1
-                connect = 1
-                break
+                return False
+
+def check_for_victory(loc):
+    loc.column -= 1
+    dist = 1 
+    connect = 1
+    print(f"Latest piece dropped into {column},{row}")
+    # Check vertical
+
     # Check horizontal
     for i in range(0, 4):
         if column+dist > 6 or array2D[row][column+dist] != player_turn:
@@ -141,12 +149,13 @@ while victory == False:
         column = int(input("Choose a column 1-7 to drop your letter into. > "))
         if column > 7 or column < 1:
             valid_reply = False
-        else: 
+        else:
             valid_reply, row = drop_piece(array2D, column, current_player)
+            location = piece_location(column, row, current_player.title, array2D)
     if current_player.turn < 4:
         print(f"Player {current_player.title} cannot win yet because they have only taken {current_player.turn} turn(s).")
     else:
-        victory = check_for_victory(array2D, row, column, current_player.title)
+        victory = check_for_victory(piece_location)
 
 print(f"Victory for {current_player.title}! The score is now {p1.score} for {p1.title} and {p2.score} for {p2.title}.")
     
