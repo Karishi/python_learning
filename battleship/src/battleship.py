100b14e921b1a6282096658edab0892eeaa6bc32
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-07-19 23:58:12 -0700

    Begun making the Game class to initialize variables.

--- battleship/src/battleship.py
+++ battleship/src/battleship.py
@@ -3,15 +3,25 @@ class player:
         self.title = title
         self.score = score
 
-class game:
-    def __init__(self, ship, board, turn):
-        self.ship = ship
-        self.board = board
-        self.turn = turn
-
 class ship:
     def __init__(self, size, coordinate, direction, state):
         self.size = size
         self.coordinate = coordinate
         self.direction = direction
         self.state = state
+
+class game:
+    title = input("What's your name? ")
+    score = 0
+    player = player(title, score)
+    cols = 10
+    rows = 10
+    header_row = [str(i) for i in range(rows+1)]
+    board = list[cols][rows]
+    for cols in board:
+        for rows in board:
+            board[cols][rows] = "O"
+    print(header_row)
+    for row in board:
+        print(row)
+game()
