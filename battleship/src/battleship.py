79e94910a1aa8980f22521d4bca6415a9e906810
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-07-23 21:39:55 -0700

    Setup can now add one test ship of a chosen length to a random location on the board.

--- battleship/src/battleship.py
+++ battleship/src/battleship.py
@@ -1,3 +1,22 @@
+import random
+
+class player:
+    def __init__(self, title, score):
+        self.title = title
+        self.score = score
+
+class ship:
+    def __init__(self, size, coordinate, direction, state):
+        self.size = int(size)
+        self.coordinate = coordinate
+        self.direction = direction
+        self.state = state
+
+class board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
 def alph_to_num(letter):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for i in range(0,len(alphabet)):
@@ -11,34 +30,56 @@ def test_alph():
     print(f"Number is {num}.")
 
 def split_coordinates(coordinate_set):
-    first_char = slice(1)
+    first_char = coordinate_set[1]
     letter = coordinate_set[first_char]
-    end_chars = slice(1,5)
+    end_chars = coordinate_set[1:]
     number = int(coordinate_set[end_chars])
     return alph_to_num(letter), number
 
 def test_split():
     user_input = "F10"
-    board, header = make_board(10,10)
+    my_board = board(10,10)
+    hidden_board, header = make_board(my_board)
     x,y = split_coordinates(user_input)
-    board[x][y-1] = "X"
-    print_board(board, header)
+    hidden_board[x][y-1] = "X"
+    print_board(hidden_board, header)
+
+def translate_direction(direction):
+    if direction.capitalize() == "N":
+        return 0,-1
+    elif direction.capitalize() == "S":
+        return 0,1
+    elif direction.capitalize() == "W":
+        return -1,0
+    elif direction.capitalize() == "E":
+        return 1,0
+    else:
+        return 0,0
+    
+def randomize_ship_start(board):
+    rand_x = random.randint(0,board.width)
+    rand_y = random.randint(0,board.height)
+    return rand_x, rand_y
+    
+def test_trans_direction():
+    direction = "N"
+    ship_length = 3
+    x_shift,y_shift = translate_direction(direction)
+    my_board = board(10,10)
+    if x_shift == 0 and y_shift == 0:
+        print("Error! Inaccurate input")
+    test_board,header = make_board(my_board)
+    x, y = randomize_ship_start(my_board)
+    for i in range(ship_length):
+        test_board[x+i*x_shift][y+i*y_shift] = "D"
+    print_board(test_board, header)
 
 
-class player:
-    def __init__(self, title, score):
-        self.title = title
-        self.score = score
 
-class ship:
-    def __init__(self, size, coordinate, direction, state):
-        self.size = size
-        self.coordinate = coordinate
-        self.direction = direction
-        self.state = state
 
-def make_board(x,y):
-    return [['o' for count in range(x)] for rows in range(y)], [str(i) for i in range(1,y+1)]
+
+def make_board(board):
+    return [['o' for count in range(board.height)] for rows in range(board.width)], [str(i) for i in range(1,board.width+1)]
 
 def print_board(board, header):
     print(' '.join(header))
@@ -48,10 +89,11 @@ def print_board(board, header):
 
 class game:
     title = input("What's your name? ")
+    my_board = board(10,10)
     score = 0
     player = player(title, score)
-    myboard, header = make_board(10,10)
-    print_board(myboard, header)
+    board, header = make_board(my_board)
+    print_board(board, header)
 
-test_split()
+test_trans_direction()
 game()
