468b328046748eec8f0f87c572779d3606e0d0bf
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-08-22 18:21:39 -0700

    Wrote 'standard' placement, putting a 2-pip Destroyer, 3-pip Submarine and Cruiser, 4-pip Battleship, and 5-pip Carrier. Tracked down bug causing edge-check to not catch out-of-bounds exceptions on upper bounds.

--- battleship/test/test_ship.py
+++ battleship/test/test_ship.py
@@ -15,15 +15,17 @@ def test_trans_direction():
     print_board(my_board.spaces, my_board.header)
 
 def test_edge():
-    myBoard = Board(10,10)
-    myShip = Ship(3, (3,1), {}, "D")
-    print(f"With coordinates 1,1 going North this returns {edge_check(myShip, myBoard)}.")
+    myBoard = Board(10,2)
+    myShip = Ship(3, (0,1), {}, "D")
+    print(f"With coordinates {myShip.coordinate} going North this returns {edge_check(myShip, myBoard)}.")
     myShip.direction = "E"
-    print(f"With coordinates 1,1 going East this returns {edge_check(myShip, myBoard)}.")
+    print(f"With coordinates {myShip.coordinate} going East this returns {edge_check(myShip, myBoard)}.")
     myShip.direction = "S"
-    print(f"With coordinates 1,1 going South this returns {edge_check(myShip, myBoard)}.")
+    print(f"With coordinates {myShip.coordinate} going South this returns {edge_check(myShip, myBoard)}.")
     myShip.direction = "W"
-    print(f"With coordinates 1,1 going West this returns {edge_check(myShip, myBoard)}.")
+    print(f"With coordinates {myShip.coordinate} going West this returns {edge_check(myShip, myBoard)}.")
+    place_ship(myShip, myBoard)
+    print_board(myBoard.spaces, myBoard.header)
 
 def test_place_ship():
     myBoard = Board(10,10)
@@ -52,4 +54,15 @@ def test_check_full_ship():
     print(f"The check of the full ship reads as {check_full_ship(crashingShip, myBoard)}")
     print_board(myBoard.spaces, myBoard.header)
 
-test_check_full_ship()
+def test_place_ship():
+    myBoard = Board(10,10)
+    myShip = Ship(4, get_unused(myBoard), {}, "B")
+    place_ship(myShip, myBoard)
+    print_board(myBoard.spaces, myBoard.header)
+
+def test_place_standard():
+    myBoard = Board(10,10)
+    fill_standard_board(myBoard)
+    print_board(myBoard.spaces, myBoard.header)
+
+test_place_standard()
