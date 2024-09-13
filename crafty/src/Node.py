c59d2dfdeadd14d6fb37155e96f22ab5b7f1311c
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-09-12 22:14:04 -0700

    Function for progressing a node complete and tested.

--- crafty/src/Node.py
+++ crafty/src/Node.py
@@ -20,7 +20,22 @@ def print_node(node):
     print(f"({node.value}) ", " ".join(node.options))
     return True
 
+def progress_node(value, element, node):
+    if element == node.truth:
+        node.options = [node.truth]
+        node.value -= value * 2
+    else:
+        node.options.remove(element)
+        node.value -= value
+    if node.value <= 0:
+        node.value = 0
+        return True
+    else:
+        return False
+
 my_node = Node(10, "A")
 my_node = randomize_node(my_node)
 my_node = initialize_nonfinal(my_node, 4)
+print_node(my_node)
+progress_node(8, "A", my_node)
 print_node(my_node)
