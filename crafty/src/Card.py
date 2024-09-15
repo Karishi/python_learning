7b68099f97c80723104ba3a3e6be7689445b66bb
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-09-14 22:43:44 -0700

    First attempt at writing a card with ducktyped play command, and standard effect function (take cost, attempt to match element, deal normal on non-match and double on match)

--- crafty/src/Card.py
+++ crafty/src/Card.py
@@ -1,6 +1,28 @@
 class Card:
-    def __init__(self, title, cost, impact) -> None:
+    def __init__(self, title, cost, impact, element) -> None:
         self.title = title
         self.cost = cost
         self.impact = impact
+        self.element = element
+
+def standard_effect(card, game, node):
+    game.time -= card.cost
+    node_complete = progress_node(card.value, card.element, node)
+    if node_complete:
+        game.num_incomplete -= 1
+        check_victory(game)
+    
+
+class Gentle_Sort:
+    stats = Card("Gentle Sort", 3, 5, "O")
+    def play(self, game, node):
+        if game.time > 50:
+            standard_effect(self, game, node)
+        else:
+            if game.status.contains("Relaxed"):
+                game.status.remove("Relaxed")
+                standard_effect(self, game, node)
+            else:
+                game.status.append("Relaxed")
+
 
