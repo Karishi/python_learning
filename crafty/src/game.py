8fab2d1970bfd80caf40716326322ffe2b03367e
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-09-20 23:06:01 -0700

    Setting up automatic node initialization to begin a round.

--- crafty/src/game.py
+++ crafty/src/game.py
@@ -1,6 +1,20 @@
+from node import initialize_full
+import random
+
+node_list = []
 class Game:
     def __init__(self) -> None:
-        self.status = []
         self.max_time = 100
         self.time = 100
         self.num_incomplete = 0
+        self.win_loss = None
+    initialize_full(random(3,5))
+
+my_game = Game
+
+
+def check_victory(game):
+    if game.num_incomplete == 0:
+        return True
+    else:
+        return None
