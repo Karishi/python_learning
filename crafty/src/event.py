73bfae8873f6fdc5ad4c0ff3cf1cd4a0651bfaf1
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-09-29 23:48:21 -0700

    Added event class for multiple events to enter a timer and take effect in chronological order

---
+++ crafty/src/event.py
@@ -0,0 +1,9 @@
+from player import Player
+
+class Event:
+    def __init__(self, time) -> None:
+        self.time_value = time
+    
+    def event_effect(event_type):
+        if event_type is "energy":
+            pass #FIX LATER
