b081ec6ac6504143d3ad39a60abd4d497c1267c9
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-09-30 23:37:14 -0700

    Created class framework for individual card types

---
+++ crafty/src/complete_cards.py
@@ -0,0 +1,22 @@
+from card import standard_effect
+
+def play_complete():
+    pass
+
+class Complete_Sort:
+    pass
+
+class Complete_Cut:
+    pass
+
+class Complete_Join:
+    pass
+
+class Complete_Mix:
+    pass
+
+class Complete_Arrange:
+    pass
+
+class Complete_Bind:
+    pass
---
+++ crafty/src/patient_cards.py
@@ -0,0 +1,22 @@
+from card import standard_effect
+
+def play_patient():
+    pass
+
+class Patient_Sort:
+    pass
+
+class Patient_Cut:
+    pass
+
+class Patient_Join:
+    pass
+
+class Patient_Mix:
+    pass
+
+class Patient_Arrange:
+    pass
+
+class Patient_Bind:
+    pass
