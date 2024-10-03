772b18c1dce65aa193d325b543aa36b891057d84
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-10-02 22:39:50 -0700

    Framework for 'deductive' card type that rewards being used to figure out a node.

--- crafty/src/deductive_cards.py
+++ crafty/src/deductive_cards.py
@@ -1,82 +1,41 @@
-from node import progress_node, node_list
-from game import check_victory, my_game
+from card import standard_effect, check_victory, progress_node
 
-class Card:
-    def __init__(self, title: str, cost: int, impact: int, element: str, bonus: int, target: int) -> None:
-        self.title = title
-        self.cost = cost
-        self.impact = impact
-        self.element = element
-        self.bonus = bonus
-        self.target = target
-
-
-def standard_effect(card, game, node):
-    game.time -= card.cost
-    node_complete = progress_node(card.value, card.element, node)
+def play_deductive(stats, game, node):
+    is_discovered = True
+    if len(node.options) == 1:
+        is_discovered = False
+    node_complete = progress_node(stats.value, stats.element, node)
     if node_complete:
         game.num_incomplete -= 1
         game.win_loss = check_victory(game)
-
-
-def play_gentle(self, game, node):
-    if game.status.contains("Relaxed"):
-        game.status.remove("Relaxed")
-        standard_effect(self, game, node)
+    if len(node.options) != 1:
+        is_discovered = False
+    if is_discovered:
+        loss_event.time += stats.cost
     else:
-        game.status.append("Relaxed")
-        temp_cost = self.cost
-        self.cost = 0
-        standard_effect(self, game, node)
-        self.cost = temp_cost
+        player_turn.time += stats.cost
+    
 
 
-def draw(hand, deck):
-    hand.append(deck.pop(0))
+class Deductive_Sort:
+    pass
 
 
-def draw_hand(hand, deck, hand_size):
-    for i in range(hand_size):
-        draw(hand, deck)
+class Deductive_Cut:
+    pass
 
 
-class Gentle_Sort:
-    stats = Card("Gentle Sort", 3, 5, "O", 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_gentle(stats, game, node)
+class Deductive_Join:
+    pass
 
 
-class Gentle_Cut:
-    stats = Card("Gentle Cut", 3, 5, "X", 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_gentle(stats, game, node)
+class Deductive_Mix:
+    pass
 
 
-class Gentle_Join:
-    stats = Card("Gentle Join", 3, 5, "D", 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_gentle(stats, game, node)
+class Deductive_Arrange:
+    pass
 
 
-class Gentle_Mix:
-    stats = Card("Gentle Mix", 3, 5, "S", 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_gentle(stats, game, node)
-
-
-class Gentle_Arrange:
-    stats = Card("Gentle Arrange", 3, 5, "A", 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_gentle(stats, game, node)
-
-
-class Gentle_Bind:
-    stats = Card("Gentle Bind", 3, 5, "Y", 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_gentle(stats, game, node)
+class Deductive_Bind:
+    pass
