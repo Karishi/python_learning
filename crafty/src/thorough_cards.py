54286eb30d38f81c86111b2fc55e9fe3ad3110ec
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-10-04 23:54:29 -0700

    Begun work on Thorough card type.

--- crafty/src/thorough_cards.py
+++ crafty/src/thorough_cards.py
@@ -1,7 +1,27 @@
-from card import standard_effect
+from card import standard_effect, progress_node
+from game import my_player
 
-def play_thorough():
-    pass
+def play_thorough(stats, game, node):
+    player_turn.time_value += stats.cost
+
+    if stats.element == node.truth:
+        node.options = [node.truth]
+        node.value -= stats.value * 2
+        my_player.energy += 3
+    elif len(node.options) == 2:
+        node.options.remove(stats.element)
+        node.value -= stats.value
+    else:
+        pass #FIX THIS
+
+    if node.value <= 0:
+        node.value = 0
+        return True
+
+    if node_complete:
+        game.num_incomplete -= 1
+        game.win_loss = check_victory(game)    
+
 
 class Thorough_Sort:
     pass
