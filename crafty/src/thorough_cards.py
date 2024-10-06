81314bf27fb7388cf7a9fa2c72f1040fb7229a63
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-10-05 20:30:35 -0700

    Handled for Thorough cards picking a random second incorrect answer to delete.

--- crafty/src/thorough_cards.py
+++ crafty/src/thorough_cards.py
@@ -2,17 +2,25 @@ from card import standard_effect, progress_node
 from game import my_player
 
 def play_thorough(stats, game, node):
+# This happens regardless of the rest.    
+    node.value -= stats.value
     player_turn.time_value += stats.cost
 
+# If the element matches, Thorough cards gain energy for the player.
     if stats.element == node.truth:
         node.options = [node.truth]
-        node.value -= stats.value * 2
+        node.value -= stats.value
         my_player.energy += 3
+# If there are exactly 2 options - correct and incorrect - Thorough is the wrong card to play and does nothing special.
     elif len(node.options) == 2:
         node.options.remove(stats.element)
-        node.value -= stats.value
+# If there are 3+ options left, guessing the incorrect element clears an extra wrong answer.
     else:
-        pass #FIX THIS
+        node.options.remove(stats.element)
+        for node in node.options:
+            if node.element != node.truth:
+                node.options.remove(node.element)
+                break
 
     if node.value <= 0:
         node.value = 0
