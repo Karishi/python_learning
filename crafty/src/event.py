7968246508ba0a6ef997636e50bcdb80a9ad3038
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-10-08 22:51:00 -0700

    Started process to initialize the timeline.

--- crafty/src/event.py
+++ crafty/src/event.py
@@ -2,7 +2,7 @@ from player import Player
 from node import print_board
 
 class Event:
-    def __init__(self, time) -> None:
+    def __init__(self, time: int) -> None:
         self.time_value = time
     
     def energy_gain(player):
@@ -14,4 +14,16 @@ class Event:
 
     def player_turn(player):
         print_board()
-        
+
+player_turn = Event(0)
+
+def initialize_timeline(duration):
+    timeline = []
+    timeline.append(player_turn)
+    time_count = 5
+    for i in range(int(duration/5)):
+        energy_event = Event(time_count)
+        time_count += 5
+        timeline.append(energy_event)
+    loss_event = Event(duration)
+    timeline.append(loss_event)
