56484884fc892c214acaa1327999995a3c9a8561
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-10-17 23:42:05 -0700

    Wrote the Complete card type.

--- crafty/src/complete_cards.py
+++ crafty/src/complete_cards.py
@@ -1,7 +1,17 @@
 from card import standard_effect
+from node import progress_node
+from game import check_victory, my_player
 
-def play_complete():
-    pass
+def play_complete(stats, game, node):
+    node_complete = progress_node(stats.value, stats.element, node)
+    if node_complete:
+        game.num_incomplete -= 1
+        game.win_loss = check_victory(game)
+        my_player.energy += 10
+    else:
+        game.time -= stats.cost
+        
+        
 
 class Complete_Sort:
     pass
