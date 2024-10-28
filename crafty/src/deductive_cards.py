1c3bec440ee76569d0e62650999486ea26736657
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-10-27 22:35:21 -0700

    Adjusted deductive to include a basic play function so it can ducktype same as Complete

--- crafty/src/deductive_cards.py
+++ crafty/src/deductive_cards.py
@@ -1,37 +1,37 @@
 from card import standard_effect, check_victory, progress_node, Card
-from game import my_game
+from game import my_game, my_player
 from node import node_list
+from event import Loss_Event
 
-def play_deductive(stats, game, node):
-    is_discovered = True
-
-# No bonus if the node is already revealed
-    if len(node.options) == 1:
-        is_discovered = False
-    
-    node_complete = progress_node(stats.value, stats.element, node)
-    
-    if node_complete:
-        game.num_incomplete -= 1
-        game.win_loss = check_victory(game)
-    
-# No bonus if the node isn't revealed by this card.
-    if len(node.options) != 1:
-        is_discovered = False
-
-# Bonus if this card reveals the node's element: It gains time instead of spending it.
-    if is_discovered:
-        loss_event.time_value += stats.cost
-    else:
-        player_turn.time_value += stats.cost
-    
-
-
-class Deductive_Sort(Card):
+class Deductive_Card(Card):
     def __init__(self, title: str, time_cost: int, impact: int, element: str, bonus_energy_cost: int, target: int, description: str) -> None:
         super().__init__(title, time_cost, impact, element, bonus_energy_cost, target, description)
-# What strikes me is that the cards can ducktype into playing via a play for each type.
-# No need for each one to have "deductive_play" or "gentle_play" for type.
+
+    def play(stats, game, node):
+        is_discovered = True
+
+    # No bonus if the node is already revealed
+        if len(node.options) == 1:
+            is_discovered = False
+        
+        node_complete = progress_node(stats.value, stats.element, node)
+        
+        if node_complete:
+            game.num_incomplete -= 1
+            game.win_loss = check_victory(game)
+        
+    # No bonus if the node isn't revealed by this card.
+        if len(node.options) != 1:
+            is_discovered = False
+
+    # Bonus if this card reveals the node's element: It gains time instead of spending it.
+        if is_discovered:
+            for item in my_game.timeline:
+                if isinstance(item, Loss_Event):
+                    time = item
+            time += stats.cost
+        else:
+            my_player.time += stats.cost
 
 
 class Deductive_Cut:
@@ -41,29 +41,3 @@ class Deductive_Cut:
     play_deductive(stats, game, node)
 
 
-class Deductive_Join:
-    stats = Card("Deductive Join", 7, 7, "D", 6, 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_deductive(stats, game, node)
-
-
-class Deductive_Mix:
-    stats = Card("Deductive Mix", 7, 7, "S", 6, 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_deductive(stats, game, node)
-
-
-class Deductive_Arrange:
-    stats = Card("Deductive Arrange", 7, 7, "A", 6, 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_deductive(stats, game, node)
-
-
-class Deductive_Bind:
-    stats = Card("Deductive Bind", 7, 7, "Y", 6, 0)
-    game = my_game
-    node = node_list[stats.target]
-    play_deductive(stats, game, node)
