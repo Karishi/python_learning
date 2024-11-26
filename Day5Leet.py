90338148f92c366b472dfb9c16ccc500f86f3579
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-11-25 17:44:14 -0800

    Working on fixing up the answer to avoid issues.

--- Day5Leet.py
+++ Day5Leet.py
@@ -1,37 +1,37 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution(object):
-    def branchDepth(root, node, depth = 0):
+    def branchDepth(root, node):
         if root is None:
             return -1
         
-        if root is node:
-            return depth
-
-        left_depth = branchDepth(root.left, node, depth +1)
-        if left_depth != -1:
-            return left_depth
+        depth = 0
+        while root is not None:
+            if root.val == node.val:
+                return depth
+            elif root.val > node.val:
+                root = root.right
+            else:
+                root = root.left
+            depth += 1
         
-        right_depth = branchDepth(root.right, node, depth +1)
-        if right_depth != -1:
-            return right_depth
-        
-    def isBalanced(self, root):
+    def isBalanced(self, tree, root):
         min_run = 2501
         max_run = 0
         
-        for node in root:
-            depth = branchDepth(root, node, depth)
-            if depth < min_run:
-                min_run = depth
-            if depth > max_run:
-                max_run = depth
-            if max_run > min_run + 1:
-                return False
+        for node in tree:
+            if node.left is None and node.right is None:
+                depth = branchDepth(root, node, depth)
+                if depth < min_run:
+                    min_run = depth
+                if depth > max_run:
+                    max_run = depth
+                if max_run > min_run + 1:
+                    return False
         return True
     
