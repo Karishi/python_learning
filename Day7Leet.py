19f5e4cbcec86676b37060bc21aee0ded3155680
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-11-27 23:13:39 -0800

    Starting to mess with linkedlist manipulation.

---
+++ Day7Leet.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        digit1 = l1[0]
+        digit2 = l2[0]
+        output = ListNode()
+        carried = 0
+        while digit1 is not None or digit2 is not None:
+            if digit1 is None:
+                digit1val = 0
+            else:
+                digit1val = digit1.val
+            if digit2 is None:
+                digit2val = 0
+            else:
+                digit2val = digit2.val
+            output.val = (digit1val + digit2val + carried) % 10
+            carried = (digit1val + digit2val + carried) // 10
+            
