bc5c99a79ab5e603da951777c2eaa2bb3fb38b24
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-12-04 22:47:01 -0800

    Added comments and put 'test equidistant values' effect inside the while loop so the algorithm doesn't miss the correct answer if it's the second-highest and second-lowest values.

--- Day12Leet.py
+++ Day12Leet.py
@@ -6,31 +6,35 @@ list = [1,2,3,4,5,6,7,8,0]
 
 def buyLowSellHigh(list):
     onlyup = sorted(list)
-    maxGains = 0
-    if list.index(onlyup[0]) < list.index(onlyup[-1]):
-        return onlyup[-1] - onlyup[0]
-    else:
-        leftside = 0
-        rightside = len(list)-1
-        offset = 1
-        success = False
-        rightBest = 0
-        leftBest = 0
-        while leftside < rightside:
-            if list.index(onlyup[leftside]) < list.index(onlyup[rightside - offset]):
-                leftBest = onlyup[rightside - offset] - onlyup[leftside]
-                success = True
-            if list.index(onlyup[leftside + offset]) < list.index(onlyup[rightside]):
-                rightBest = onlyup[rightside] - onlyup[leftside + offset]
-                success = True
-            if success:
-                if rightBest > leftBest:
-                    return rightBest
-                else:
-                    return leftBest
-            leftside += 1
-            rightside -= 1
-        return 0
+    leftside = 0
+    rightside = len(list)-1
+    offset = 1
+    success = False
+    rightBest = 0
+    leftBest = 0
+
+    while leftside < rightside:
+        # Finds the smallest and largest value in the list.
+        # Checks whether they're in the right order.
+        if list.index(onlyup[leftside]) < list.index(onlyup[rightside]):
+            return onlyup[rightside] - onlyup[leftside]
+        # If not, compares the second-smallest to largest vs smallest to second-largest
+        # If one of them is in the right order, the one with the greatest value is the answer.
+        if list.index(onlyup[leftside]) < list.index(onlyup[rightside - offset]):
+            leftBest = onlyup[rightside - offset] - onlyup[leftside]
+            success = True
+        if list.index(onlyup[leftside + offset]) < list.index(onlyup[rightside]):
+            rightBest = onlyup[rightside] - onlyup[leftside + offset]
+            success = True
+        if success:
+            if rightBest > leftBest:
+                return rightBest
+            else:
+                return leftBest
+        # If not, remove the highest and lowest from the search area and move toward the middle.
+        leftside += 1
+        rightside -= 1
+    return 0
 
                 
         
