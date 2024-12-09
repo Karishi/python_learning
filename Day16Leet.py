b571cb546c72332abbef9c449e1cc54bf744a44a
Jeremy Anderson <jeremyanderson@Jeremys-iMac.local>
2024-12-08 20:27:24 -0800

    Make a function that returns a square root rounded down to the nearest integer, without using any built-in exponent functions (such as ** in Python).

---
+++ Day16Leet.py
@@ -0,0 +1,12 @@
+def mySqrt(x):
+    attempt = 1
+    while x/attempt > attempt:
+        attempt += 1
+    if attempt > x/attempt:
+        return attempt-1
+    else:
+        return attempt
+
+test = 148
+
+print(mySqrt(test))
