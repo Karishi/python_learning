609248d77dbdd4a0efa2e9be4721e3b1b9c72be6
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2024-12-13 21:56:53 -0800

    Fixed issue where checks to add further cells were based in incorrect inputs.

--- Day20Leet.py
+++ Day20Leet.py
@@ -11,46 +11,47 @@ def findExteriorWater(matrixList):
     seen = []
     if 0 in matrixList[0]:
         for node in range(len(matrixList)):
-            if node == 0:
+            if matrixList[0][node] == 0:
                 processList.append(waterNode(node, 0))
-                seen.append(waterNode(node, 0))
-    for space in range(1, len(matrixList)-2):
+                seen.append((node, 0))
+    for space in range(1, len(matrixList)-1):
         if matrixList[space][0] == 0:
             processList.append(waterNode(0, space))
-            seen.append(waterNode(0, space))
+            seen.append((0, space))
         if matrixList[space][-1] == 0:
             processList.append(waterNode(len(matrixList[space])-1, space))
-            seen.append(waterNode(len(matrixList[space])-1, space))
+            seen.append((len(matrixList[space])-1, space))
     if 0 in matrixList[-1]:
         for node in range(len(matrixList)):
-            if node == 0:
+            if matrixList[-1][node] == 0:
                 processList.append(waterNode(node, len(matrixList[space])-1))
-                seen.append(waterNode(node, len(matrixList[space])-1))
+                seen.append((node, len(matrixList[space])-1))
+
+
     while len(processList) > 0:
         waterTotal += 1
-        current = processList.pop()
+        current = processList.pop(0)
         x, y = current.x, current.y
-        print(f"seen contains {seen}")
         # Check East
-        if x < len(matrixList[y])-1 and matrixList[y][x+1] == 1:
+        if x < len(matrixList[y])-1 and matrixList[y][x+1] == 0:
             if (x+1, y) not in seen:
                 seen.append((x+1, y))
                 node = waterNode(x+1, y)
                 processList.append(node)
         # Check South
-        if y < len(matrixList)-1 and matrixList[y+1][x] == 1:
+        if y < len(matrixList)-1 and matrixList[y+1][x] == 0:
             if (x, y+1) not in seen:
                 seen.append((x, y+1))
                 node = waterNode(x, y+1)
                 processList.append(node)
         # Check West
-        if x > 0 and matrixList[y][x-1] == 1:
+        if x > 0 and matrixList[y][x-1] == 0:
             if (x-1, y) not in seen:
                 seen.append((x-1, y))
                 node = waterNode(x-1, y)
                 processList.append(node)
         # Check North
-        if y > 0 and matrixList[y-1][x] == 1:
+        if y > 0 and matrixList[y-1][x] == 0:
             if (x, y-1) not in seen:
                 seen.append((x, y-1))
                 node = waterNode(x, y-1)
