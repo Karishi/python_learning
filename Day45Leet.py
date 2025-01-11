c853286efd262285ee7289f5b24eea390ef721b3
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-01-11 00:06:33 -0800

    Convert roman numerals to integers. This version fixes 4s and 9s by translating them into something easily iterated over

--- Day45Leet.py
+++ Day45Leet.py
@@ -1,51 +1,30 @@
 def roman_to_int(roman):
     total = 0
-    for digit in range(len(roman)):
-        if roman[digit] == "I":
-            if digit < len(roman)-1:
-                if roman[digit+1] == "V":
-                    digit += 1
-                    total += 4
-                elif roman[digit+1] == "X":
-                    digit += 1
-                    total += 9
-                else:
-                    total += 1
-            else: 
-                total += 1
-        if roman[digit] == "X":
-            if digit < len(roman)-1:
-                if roman[digit+1] == "L":
-                    digit += 1
-                    total += 40
-                elif roman[digit+1] == "C":
-                    digit += 1
-                    total += 90
-                else:
-                    total += 10
-            else:
-                total += 10
-        elif roman[digit] == "C":
-            if digit < len(roman)-1:
-                if roman[digit+1] == "D":
-                    digit += 1
-                    total += 400
-                elif roman[digit+1] == "M":
-                    digit += 1
-                    total += 900
-                else:
-                    total += 100
-            else: total += 100
-        elif roman[digit] == "M":
-            total += 1000
-        elif roman[digit] == "D":
-            total += 500
-        elif roman[digit] == "L":
-            total += 50
-        elif roman[digit] == "V":
-            total += 5
+
+    # Translate 4s and 9s
+    roman = roman.replace("IV", "IIII")
+    roman = roman.replace("IX", "VIIII")
+    roman = roman.replace("XL", "XXXX")
+    roman = roman.replace("XC", "LCCCC")
+    roman = roman.replace("CD", "CCCC")
+    roman = roman.replace("CM", "DCCCC")
+
+    # Set up the values for use
+    trans = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    for digit in roman:
+        total += trans[digit]
+
     return total
 
-test = "MCM"
+test = "MCMLXXXII"
 
 print(roman_to_int(test))
