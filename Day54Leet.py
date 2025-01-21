64f027d7a7682922b2262787aae8f58d36855b2a
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-01-21 15:27:30 -0800

    Completed previous WIP. Now tests for row, column, and square. If it returns False, it defines what failure first caused it to return False.

--- Day54Leet.py
+++ Day54Leet.py
@@ -1,22 +1,42 @@
 def isValidSudoku(board):
+    squareList = []
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            base = []
+            for k in range(3):
+                for l in range(3):
+                    base.append(board[i + k][j + l])
+            squareList.append(base)
+
     def testRow(index):
         row = board[index[0]]
-        if row[index[1]] != "." and row[index[1]] in row[index[1]+1:]:
+        if row[index[1]] != "." and row[index[1]] in row[index[1] + 1:]:
             return False
     
     def testCol(index):
-        for row in range(index[1]+1, len(board)-1):
-            if board[index[0]][index[1]] != "." and board[row][index[1]] == board[index[0]][index[1]]:
+        for col in range(index[0] + 1, len(board)):
+            if board[index[0]][index[1]] != "." and board[col][index[1]] == board[index[0]][index[1]]:
                 return False
     
     def testSqu(index):
-        pass
+        row = squareList[index[0]]
+        if row[index[1]] != "." and row[index[1]] in row[index[1] + 1:]:
+            return False
 
-    if testRow((0,0)) == False:
-        return False
-    
-    if testCol((0,0)) == False:
-        return False
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] != ".":
+                if testRow((i, j)) == False:
+                    print("Found false on Rows")
+                    return False
+                
+                if testCol((i, j)) == False:
+                    print("Found false on Columns")
+                    return False
+
+                if testSqu((i, j)) == False:
+                    print("Found false on Squares")
+                    return False
     
     return True
 
@@ -27,7 +47,7 @@ board = [
 ,["8",".",".",".","6",".",".",".","3"]
 ,["4",".",".","8",".","3",".",".","1"]
 ,["7",".",".",".","2",".",".",".","6"]
-,[".5","6",".",".",".",".","2","8","."]
+,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]
 ]
