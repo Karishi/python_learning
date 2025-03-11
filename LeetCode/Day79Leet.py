344b4d751e34a796a3fd3bc08165ae3704f252f3
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-03-10 19:21:34 -0700

    Updated to test in both directions so that repetition in either direction is caught.

--- LeetCode/Day79Leet.py
+++ LeetCode/Day79Leet.py
@@ -5,9 +5,15 @@ def isIsomorphic(s, t):
             return False
         else:
             used[s[i]] = t[i]
+    used = {}
+    for i in range(len(t)):
+        if t[i] in used and used[t[i]] != s[i]:
+            return False
+        else:
+            used[t[i]] = s[i]
     return True
 
-s = "foo"
-t = "bar"
+s = "tumblr"
+t = "eagles"
 
 print(isIsomorphic(s, t))
