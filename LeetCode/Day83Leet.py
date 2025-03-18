1e73bbbaa54d688690430ce9d03cac0e963faba4
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-03-17 20:31:21 -0700

    For a number n return the nth number in a list of all numbers that have prime factors of only 2, 3, and 5.

--- LeetCode/Day83Leet.py
+++ LeetCode/Day83Leet.py
@@ -1,11 +1,24 @@
-def findDuplicate(nums):
-    numberMap = {}
-    for num in nums:
-        if num in numberMap:
-            return num
-        else:
-            numberMap[num] = True
+def nthUglyNumber(n):
+    def isUgly(num):
+        while num != 1:
+            if num % 2 != 0 and num % 5 != 0 and num % 3 != 0:
+                return False
+            if num % 2 == 0:
+                num = num // 2
+            if num % 3 == 0:
+                num = num // 3
+            if num % 5 == 0:
+                num = num // 5
+        return True
+    
+    numUglies = 0
+    i = 0
+    while numUglies < n:
+        i += 1
+        if isUgly(i):
+            numUglies += 1
+    return i
 
-test = [3,1,3,4,2]
+test = 9
 
-print(findDuplicate(test))
+print(nthUglyNumber(test))
