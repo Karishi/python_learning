26814dc615c2d42512394a013654f173f1d1a14d
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-03-21 23:00:52 -0700

    Made another solution - this one creating a list that track s each value on the understanding that the maximum possible is the length of the list (the number of papers). If a value is reached that is higher than the current best it's checked to see whether it increases the number at that range to a point where it can become the new best.

--- LeetCode/Day85Leet.py
+++ LeetCode/Day85Leet.py
@@ -1,11 +1,23 @@
 def hIndex(citations):
-    citations.sort()
+    # citations.sort()
+    # best = 0
+    # for i in range(len(citations)):
+    #     if len(citations[-(i+1):]) <= citations[-(i+1)]:
+    #         best = min(len(citations[-(i+1):]), citations[-(i+1)])
+    #     else:
+    #         return best
+    # return best
     best = 0
+    listed = [0 for x in range(len(citations))]
     for i in range(len(citations)):
-        if len(citations[i:]) >= citations[i]:
-            best = min(len(citations[i:]), citations[i])
+        if citations[i] > best:
+            for j in range(min(citations[i], len(citations))):
+                print(listed, best)
+                listed[j] += 1
+                if listed[j] > j:
+                    best = min(listed[j], j+1)
     return best
 
-citations = [3,0,6,1,5]
+citations = [1,1,1,3]
 
 print(hIndex(citations))
