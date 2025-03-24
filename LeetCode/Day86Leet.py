da5678b8a5e0c93aee6e1f7aa884111879453de0
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-03-23 22:57:34 -0700

    For a list of integers, find the longest strictly increasing subsequence. That is, skipping any values that aren't higher, find the length of the longest sub-list of in-order elements that is inherently 'sorted ascending.' This solution performs recursion to follow the rising start index and maintain the count each time it finds a new higher point, then a basic for list to test each final value against a running best.

---
+++ LeetCode/Day86Leet.py
@@ -0,0 +1,23 @@
+def lengthOfLIS(nums):
+    best = 0
+
+    def recurse_higher(index, count, nums):
+        if index == len(nums)-1:
+            return count + 1
+        
+        if nums[index] < nums[index+1]:
+            count = recurse_higher(index+1, count+1, nums)
+        else:
+            count = recurse_higher(index + 1, count, nums)
+        return count
+    
+    for i in range(len(nums)):
+        count = recurse_higher(i, 0, nums)
+        if count > best:
+            best = count
+
+    return best
+
+test = [0,1,0,3,2,3]
+
+print(lengthOfLIS(test))
