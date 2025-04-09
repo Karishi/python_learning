b94850f73c4612a4ca2545132eb15a1c824aaf12
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-04-08 21:22:02 -0700

    Added asteroids. No collision detection yet. Also, shifted DeltaTime value into the clock.tick from the constants.

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,18 +3,43 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    field = AsteroidField()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill((0,0,0))
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
