ddb092b84f2939a7e5e3c93549992380132da923
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-04-10 21:10:43 -0700

    Added a shot class to permit the player to fire blasts to break asteroids. Shots can't yet interact with asteroids and aren't limited in usage.

--- asteroids/player.py
+++ asteroids/player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_SPEED
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -36,6 +37,12 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt/2)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        pew = Shot(self.position.x, self.position.y)
+        pew.velocity = pygame.Vector2(0,1).rotate(self.rotation) * SHOT_SPEED
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
