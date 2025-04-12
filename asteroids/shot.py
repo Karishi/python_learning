746a1f20ce620d0fce48d7cd1dc1369239a7ce6c
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-04-11 23:32:41 -0700

    Added a kill command to shots so they self-terminate, preventing processing from getting bloated over time.

--- asteroids/shot.py
+++ asteroids/shot.py
@@ -5,9 +5,13 @@ from constants import SHOT_RADIUS
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
+        self.duration = 0
 
     def draw(self, screen):
         pygame.draw.circle(screen, "lightblue", self.position, self.radius, 2)
     
     def update(self, dt):
         self.position += self.velocity * dt
+        self.duration += 1
+        if self.duration >= 100:
+            self.kill()
