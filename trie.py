539b179ecc7ac27f90c6747b0fd15b030de063a1
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-06-04 21:39:57 -0700

    A sample Trie in Python. WIP.

---
+++ trie.py
@@ -0,0 +1,13 @@
+class Trie:
+    def add(self, word):
+        current_level = self.root
+        for char in word:
+            if char not in current_level:
+                current_level[char] = {}
+            current_level = current_level[char]
+        current_level[self.end_symbol] = True
+                       
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
