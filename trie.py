6297db9f27b1d87d693dd157d83325434d576c1c
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-06-05 17:10:35 -0700

    Added 'exists' function to determine whether a word is present in the trie. WIP.

--- trie.py
+++ trie.py
@@ -6,6 +6,16 @@ class Trie:
                 current_level[char] = {}
             current_level = current_level[char]
         current_level[self.end_symbol] = True
+    
+    def exists(self, word):
+        current = self.root
+        for char in word:
+            if char not in current:
+                return False
+            current = current[char]
+        if self.end_symbol in current:
+            return True
+        return False
                        
 
     def __init__(self):
