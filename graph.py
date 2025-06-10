1fed52da9aee38adea96358cb22adafd67a7aea4
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-06-09 21:08:26 -0700

    Sample of a graph (including a standard initialization of a 2d array) with some basic functions to add edges and test whether a specific edge is present in the graph. WIP.

---
+++ graph.py
@@ -0,0 +1,17 @@
+class Graph:
+    def __init__(self, num_vertices):
+        self.graph = [ [False]*num_vertices for i in range(num_vertices)]
+
+    def add_edge(self, u, v):
+        self.graph[u][v] = True
+        self.graph[v][u] = True
+
+    def edge_exists(self, u, v):
+        if u < 0 or u >= len(self.graph):
+            return False
+        if len(self.graph) == 0:
+            return False
+        row1 = self.graph[0]
+        if v < 0 or v >= len(row1):
+            return False
+        return self.graph[u][v]
